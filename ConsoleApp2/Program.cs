644614b4d3e203c347ca296e54bd312cf268e741
Виктор Редькин <Moridin830@gmail.com>
2021-11-16 23:25:36 +0300

    Задание 3.2.8

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,7 +9,11 @@ class MainClass
 		myName = "Moridin830";
 
 		Console.WriteLine(myName);
-
+		Console.WriteLine("\t Привет Мир");
+		Console.WriteLine("\t Мне 31 годик");
+		Console.WriteLine("\t My name is \n {0}", myName);
+		Console.WriteLine('\u0040');
+        Console.WriteLine('\u0023');
 		Console.ReadKey();
 	}
 }
