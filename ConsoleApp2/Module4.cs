616126df25e9289ab8f06e43b553bf3eb3d65c9b
Виктор Редькин <Moridin830@gmail.com>
2021-11-22 22:24:45 +0300

    Переименование файла 3 модуля.

--- ConsoleApp2/Module4.cs
+++ ConsoleApp2/Module4.cs
@@ -77,4 +77,41 @@ class MainClass
 				break;
 		}
 	}
+
+	public static void Task4_2_1()
+    {
+		for (int i = 1; i < 5; i++)
+		{
+			Console.WriteLine("Iteration {0}", i);
+			switch (Console.ReadLine())
+			{
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is red!");
+					break;
+
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is green!");
+					break;
+
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is yellow!");
+					break;
+			}
+		}
+	}
 }
