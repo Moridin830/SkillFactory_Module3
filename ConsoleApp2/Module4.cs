fd4d069f99485b4a72dcfdda93658cfacef5c51e
Виктор Редькин <Moridin830@gmail.com>
2021-11-22 21:55:48 +0300

    Задание 4.1.18.

--- ConsoleApp2/Module4.cs
+++ ConsoleApp2/Module4.cs
@@ -39,23 +39,42 @@ class MainClass
 	}
 
 
-	public static void Task3_3()
+	public static void Task4_1_18()
 	{
 
-		string MyName = "Moridin830";
-		byte MyAge = 31;
-		bool HaveIApet = true;
-		double MyShoeSize = 42.5;
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
-	}
+		var color = Console.ReadLine();
 
-	public static void Task3_6()
-	{
-		double result = 5 / 2;
-		Console.WriteLine("5 / 2 = {0}", result);
+		switch (color)
+		{
+			case "red":
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+				break;
+
+			case "green":
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+				break;
+
+			case "cian":
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+				break;
+
+			default:
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Red;
+
+				Console.WriteLine("Your color is cyan!");
+				break;
+		}
 	}
 }
